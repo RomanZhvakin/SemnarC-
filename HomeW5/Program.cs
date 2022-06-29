989c80f11acf5e8b5f57294c1c0ff1fe859d3b04
Roman <roman_zhvakin@mail.ru>
2022-06-29 14:39:28 +0700

    Решение 10 задачи ко второму семинару

--- HomeW5/Program.cs
+++ HomeW5/Program.cs
@@ -1,2 +1,5 @@
-﻿int[] array = {4, 5, 6};
-Console.WriteLine(array[1]);
+﻿Console.Clear();
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
