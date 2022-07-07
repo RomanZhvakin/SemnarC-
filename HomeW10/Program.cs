61956073fd811b090a3c78b35d0b3a81cc4941f4
Roman <roman_zhvakin@mail.ru>
2022-07-07 12:44:47 +0700

    Решение задания 23 третий семинар

---
+++ HomeW10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int j = 1;
+while (Math.Abs(num) - j >= 0)
+{
+    Console.Write($"{j * j * j},");
+    j++;
+}
+System.Console.Write("\b \b");
