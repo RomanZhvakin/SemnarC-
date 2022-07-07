35ca549dc608c97e52644fa7778955434fee8a5e
Roman <roman_zhvakin@mail.ru>
2022-07-07 11:36:35 +0700

    Решение задания 21 третья лекция

---
+++ HomeW9/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+System.Console.WriteLine("Введите координаты точки A через запятую: ");
+string A = Console.ReadLine();
+var a1 = A.Split(',').Select(int.Parse).ToArray();
+System.Console.WriteLine("Введите координаты точки B через запятую: ");
+string B = Console.ReadLine();
+var b1 = B.Split(',').Select(int.Parse).ToArray();
+double d = Math.Sqrt(Math.Pow(b1[0] - a1[0], 2)) + (Math.Pow(b1[1] - a1[1], 2)) + (Math.Pow(b1[2] - a1[2], 2));
+System.Console.WriteLine($"Расстояние между точками:{d}");
