c9a7e4aa1ff06e5b6d92147921715ee559fd2545
Roman <roman_zhvakin@mail.ru>
2022-07-09 01:42:42 +0700

    Решение задачи 29 четвертый семинар

---
+++ HomeW13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+Console.WriteLine("Введите числа через запятую");
+string[] arr = Console.ReadLine().Split();
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i == arr.Length - 1)
+    {
+        Console.Write(arr[i]);
+    }
+    else
+    {
+        Console.Write(arr[i]);
+    }
+}
+Console.WriteLine("]");
